0c258121c377f5b767ceb68ee56e1b39469a811d
Heider Oliveira <heider.o@icloud.com>
2025-05-20 18:24:47 -0300

    Initialize Spring Boot project with Maven configuration
    Set up a new Spring Boot application with Maven as the build tool, including initial project structure, dependencies, and configurations. Added essential files such as `pom.xml`, `application.properties`, `.gitignore`, and Maven wrapper scripts for build and dependency management.

---
+++ src/main/java/com/api/rabbitmq/desafiopanapiclentes/DesafioPanApiClentesApplication.java
@@ -0,0 +1,13 @@
+package com.api.rabbitmq.desafiopanapiclentes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioPanApiClentesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioPanApiClentesApplication.class, args);
+    }
+
+}
